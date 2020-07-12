8280b0a32a11655e6648278d9ee3c7942d5abb6a
David B Hon <davidb.hon@gmail.com>
2020-07-12 15:59:45 -0500

    sync

--- fmpcloud.py
+++ fmpcloud.py
@@ -1,1 +1,14 @@
-59905dc223dae03bb6d76fc0c201433c
+#!/usr/bin/env python
+from pyfmpcloud import settings
+from pyfmpcloud import stock_time_series as sts
+
+fmpc_key = '59905dc223dae03bb6d76fc0c201433c'
+
+settings.set_apikey(fmpc_key)
+
+tickr = 'AAPL'
+
+rtq = sts.real_time_quote(tickr)
+
+print(rtq)
+
